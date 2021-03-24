3bd9aa8584bd1f62b9070cd7d8f25a87604915de
Daniel151505 <daniel150520011@gmail.com>
2021-03-24 14:51:59 -0500

    Agregando sentencias if y else

--- Sentencias de Control/sentencias-if-else.py
+++ Sentencias de Control/sentencias-if-else.py
@@ -1,7 +1,12 @@
 condicion = False
 
-
-
+if condicion == True:
+    print("La condición es verdadera")
+elif condicion == False:
+    print("La condición es falsa")
+else:
+    print("La condición no es reconocida")
+    
 numero = int(input("Proporciona un número entre 1 y 3:"))
 
 
