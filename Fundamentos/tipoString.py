730ee09130ed31d13e29a1d7e59c6b27c8ce53c7
Daniel151505 <daniel150520011@gmail.com>
2021-03-23 22:07:08 -0500

    Concatenando de otra forma

--- Fundamentos/tipoString.py
+++ Fundamentos/tipoString.py
@@ -4,8 +4,8 @@ print("Esto es una " + cadena)
 
 numero1 = "1"
 numero2 = "2"
-print("Concatencacion " , numero1 + numero2)
+print("Concatencacion:" , numero1 + numero2)
 
 numero1 = 1
 numero2 = 2
-print(numero1 + numero2)
+print("Operacion suma: " + str(numero1 + numero2))
