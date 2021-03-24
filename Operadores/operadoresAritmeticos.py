9be78047088ad1e0d5ccc4edcf6cad05112a7989
Daniel151505 <daniel150520011@gmail.com>
2021-03-23 22:43:23 -0500

    Operador Modulo

--- Operadores/operadoresAritmeticos.py
+++ Operadores/operadoresAritmeticos.py
@@ -4,8 +4,11 @@ suma = a + b
 resta = a - b
 multi = a * b
 division = a / b
+modulo = a % b
 
 print("suma:" , suma)
 print("resta:" , resta)
 print("multiplicacion:" , multi)
 print("division:", division)
+print("modulo:", modulo)
+
