2f73f5b9054eef8156dc9ddd86aebab158906b48
Daniel151505 <daniel150520011@gmail.com>
2021-03-24 13:50:38 -0500

    Agregando Variables

---
+++ Ejercicios/tiendaLibros.py
@@ -0,0 +1,7 @@
+print("Proporcione los siguientes datos del libro")
+nombre = input("Proporciona el nombre:")
+
+id = int (input("Proporcione el ID:"))
+precio = float(input("Proporcione el precio:"))
+envioGratuito = input("Indica si el envio es gratuito(True/False):")
+
