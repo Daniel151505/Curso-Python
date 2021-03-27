0e5570833123d3a23d50065c89f0708e472ef546
Daniel151505 <daniel150520011@gmail.com>
2021-03-27 12:45:11 -0500

    Agregando y imprimiendo set

--- Colecciones en Python/Set.py
+++ Colecciones en Python/Set.py
@@ -1,2 +1,4 @@
 #set es una colección sin orden y sin indice, no permite elementos repetidos
 #  los elementos no se pueden modificar, pero si agregar nuevos o eliminar
+planetas = {"Marte", "Jupiter", "Venus"}
+print(planetas)
