f597443fc187925473429f3010564fa3ba7af583
Daniel151505 <daniel150520011@gmail.com>
2021-03-26 22:52:31 -0500

    Comentando

--- Ciclos/break-continue.py
+++ Ciclos/break-continue.py
@@ -1,8 +1,10 @@
 #Imprimir solo las letras a 
 
-for letra in "Holanda":
-    if letra == "a"
-        print(letra)
-        break
-    else:
-    print("Fin ciclo for")
+#for letra in "Holanda":
+#    if letra == "a"
+#        print(letra)
+#        break
+#    else:
+#    print("Fin ciclo for")
+    
+#Imprimir solo números pares
