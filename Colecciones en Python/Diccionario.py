5d6c00140175c0d7f9c918c5cfd783553119237a
Daniel151505 <daniel150520011@gmail.com>
2021-03-27 12:59:15 -0500

    Agregando Diccionario

--- Colecciones en Python/Diccionario.py
+++ Colecciones en Python/Diccionario.py
@@ -1,2 +1,7 @@
 #un diccionario esta compuesto de llave, valor (key, value)
+diccionario = {
+    "IDE":"Integrated Development Environment",
+    "OOP": "Object Oriented Programing",
+    "DBMS": "Database Management System"
+}
 
