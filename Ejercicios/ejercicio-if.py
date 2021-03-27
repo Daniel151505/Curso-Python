cb71095d0727bcb7fa751f44b6e967847bf98bef
Daniel151505 <daniel150520011@gmail.com>
2021-03-26 22:17:23 -0500

    Fix error

--- Ejercicios/ejercicio-if.py
+++ Ejercicios/ejercicio-if.py
@@ -7,7 +7,7 @@ elif mes == 3 or mes == 4 or mes == 5:
     estacion = "Primavera"
 elif mes == 6 or mes == 7 or mes == 8:
     estacion = "Verano"
-elif mes == 9 or mes == 10 or mes == 11
+elif mes == 9 or mes == 10 or mes == 11:
     estacion = "Otoño"
 else: 
     estacion = "Mes incorrecto"
